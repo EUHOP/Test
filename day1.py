727ea613e82bbdfa24f82074cb20509375f74f8b
EUHOP <uga.inde@gmail.com>
2026-08-12 10:06:19 +0530

    this is a day 1 practice file

---
+++ day1.py
@@ -0,0 +1,11 @@
+#day1 variabales creations
+lead_name = "Riya Sharma"
+lead_age = 16
+lead_city = "Bangalore"
+lead_french_level = "A2"
+is_qualified_lead = True
+print (lead_name)
+print (lead_age)
+print (lead_city)
+print (lead_french_level)
+print (is_qualified_lead)
